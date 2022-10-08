52d649cedd7be1f1bbdba8b277fea0f6e8db9e7f
Adam Yao <adamyao3240@gmail.com>
2022-10-08 00:10:05 -0400

    email/password working. firebase working. will save to firebase.

--- JacketStash/JacketStash/ContentView.swift
+++ JacketStash/JacketStash/ContentView.swift
@@ -6,21 +6,117 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous).foregroundStyle(.linearGradient(colors: [.pink, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 1000, height: 400)
+                .rotationEffect(.degrees(135))
+                .offset(y:-350)
+            
+            VStack(spacing: 20){
+                Text("Welcome")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .rounded))
+                    .offset(x:-100, y:-100)
+                
+                TextField("Email", text:$email)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: email.isEmpty){
+                        Text("Email")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width:350, height:1)
+                    .foregroundColor(.white)
+                SecureField("Password", text: $password)
+                    .foregroundColor(.white)
+                    .textFieldStyle(.plain)
+                    .placeholder(when: password.isEmpty){
+                        Text("Password")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                
+                Rectangle()
+                    .frame(width:350, height:1)
+                    .foregroundColor(.white)
+                
+                Button {
+                    register()
+                } label: {
+                    Text("Sign up")
+                        .bold()
+                        .frame(width:200, height: 40)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10, style:.continuous).fill(.linearGradient(colors: [.pink, .red], startPoint: .top, endPoint: .bottomTrailing))
+                        )
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 110)
+                
+                Button {
+                    login()
+                } label: {
+                    Text("Already have an account? Login")
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .padding(.top)
+                .offset(y: 110)
+                
+                
+            }
+            .frame(width: 350)
+            .offset(y: -10)
+            
         }
-        .padding()
+        .ignoresSafeArea()
+        
+        
+    }
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) {
+            result, error in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if error != nil {
+                print(error!.localizedDescription)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
+        }
     }
 }
--- JacketStash/JacketStash/JacketStashApp.swift
+++ JacketStash/JacketStash/JacketStashApp.swift
@@ -6,29 +6,19 @@
 //
 
 import SwiftUI
-import FirebaseCore
-
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
+import Firebase
 
 @main
 struct JacketStash: App {
   // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-
+  
+    init() {
+        FirebaseApp.configure()
+    }
 
   var body: some Scene {
     WindowGroup {
-      NavigationView {
         ContentView()
-      }
     }
   }
 }
