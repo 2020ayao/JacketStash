36c27e613deaac255e37d66256f50b5d4cf42e19
Adam Yao <adamyao3240@gmail.com>
2022-10-07 21:02:28 -0400

    Init commit 2

---
+++ JacketStash/JacketStash/JacketStashApp.swift
@@ -0,0 +1,34 @@
+//
+//  JacketStashApp.swift
+//  JacketStash
+//
+//  Created by Adam Yao on 10/7/22.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct JacketStash: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
